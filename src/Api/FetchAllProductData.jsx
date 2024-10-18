56d369b9d575953d9306e8c0d0fb56b7daf56112
Vitaliy Milvus <shylika@i.ua>
2024-10-18 20:19:50 +0300

    FIX: build

--- src/Api/FetchAllProductData.jsx
+++ src/Api/FetchAllProductData.jsx
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 const baseUrl = 'https://graming.com';
 const consumer_key = process.env.REACT_APP_CONSUMER_KEY;
-console.log(consumer_key);
-
 const consumer_secret = process.env.REACT_APP_CONSUMER_SECRET;
 
 const FetchAllProductData = async (
