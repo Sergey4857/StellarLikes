1be19fabc0164e3638798d54bc148b0b6c916381
Sergey4857 <yasak.sergey@gmail.com>
2024-10-21 22:18:15 +0300

    ++

--- src/components/Intercom/Intercom.jsx
+++ src/components/Intercom/Intercom.jsx
@@ -12,14 +12,12 @@ export default function Component() {
   useEffect(() => {
     console.log('Initializing Intercom...');
     Intercom('boot', {
-      app_id: 'lk2bf6cz', // Проверьте правильность app_id
+      app_id: 'lk2bf6cz',
       user_id: user.id,
       name: user.name,
       email: user.email,
       created_at: user.createdAt,
     });
-
-    Intercom('onShow', () => console.log('Intercom widget is shown!'));
 
     return () => {
       console.log('Shutting down Intercom...');
