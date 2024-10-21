118d9b3fcd86c265bab161ced6badb9dc553fde6
Sergey4857 <yasak.sergey@gmail.com>
2024-10-21 22:01:42 +0300

    ++

--- api.php
+++ api.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 header('Content-Type: application/json');
 
 // CORS Headers
-header("Access-Control-Allow-Origin: *"); // 
+header("Access-Control-Allow-Origin: https://stellarlikes.com"); // 
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
