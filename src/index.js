95428658b937032d21b60c106533f1fc568bbd6d
Sergey4857 <yasak.sergey@gmail.com>
2024-10-22 13:06:15 +0300

    ++

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import { BrowserRouter } from 'react-router-dom';
-import App from 'components/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { BrowserRouter } from 'react-router-dom';
+import App from 'components/App';
+import { IntercomProvider } from 'react-use-intercom'; // Import IntercomProvider
 import './index.css';
+const INTERCOM_APP_ID = 'lk2bf6cz';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <IntercomProvider appId={INTERCOM_APP_ID} autoBoot>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </IntercomProvider>
 );
